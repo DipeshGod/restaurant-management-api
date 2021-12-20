ebf17c7ccbdc0777b6c32a6f0f33ebcc079d1ad2
CodeAddictDipesh <dipeshonblack@gmail.com>
2021-12-20 18:19:06 +0545

    authorization middleware created

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Response, Request } from 'express';
+import { User } from '../models/User';
+
+const isOwner = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.user._id;
+
+  const user = await User.findById(id);
+
+  if (user?.role.includes('owner')) {
+    next();
+  } else {
+    return res.status(401).json({
+      msg: 'Your are not authorize to access this page',
+    });
+  }
+};
+
+export { isOwner };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import { createUserController } from '../controllers/user/createUserController';
 import { getUserController } from '../controllers/user/getUserController';
 import { authentication } from '../middlewares/authenticate';
+import { isOwner } from '../middlewares/authorization';
 
 const router = Router();
 
-router.get('/', authentication, getUserController);
-router.post('/', createUserController);
+router.get('/', [authentication, isOwner], getUserController);
+router.post('/', [authentication, isOwner], createUserController);
 
 export { router as userRouter };
