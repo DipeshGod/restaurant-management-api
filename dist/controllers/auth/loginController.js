cd232b703ee41cebe633e0753ceda9371f87ba57
CodeAddictDipesh <dipeshonblack@gmail.com>
2022-02-24 15:54:19 +0545

    build

--- dist/controllers/auth/loginController.js
+++ dist/controllers/auth/loginController.js
@@ -54,6 +54,17 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -82,38 +93,43 @@ var assignToken = function (user) {
     return token;
 };
 var loginController = function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var user, isValidPassword, token, err_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
+    var user, isValidPassword, token, _a, password, userWithoutPassword, err_1;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
             case 0:
-                _a.trys.push([0, 4, , 5]);
+                _b.trys.push([0, 4, , 5]);
                 //validate the request data
                 return [4 /*yield*/, (0, authValidator_1.loginValidator)(req.body)];
             case 1:
                 //validate the request data
-                _a.sent();
+                _b.sent();
                 return [4 /*yield*/, User_1.User.findOne({
                         mobileNumber: req.body.mobileNumber,
                         restaurant: req.body.restaurant,
                     }).populate('restaurant')];
             case 2:
-                user = _a.sent();
+                user = _b.sent();
                 //if user is not found
                 if (!user) {
                     return [2 /*return*/, res.status(401).json({ msg: 'Invalid Credentials' })];
                 }
                 return [4 /*yield*/, validatePassword(user.password, req.body.password)];
             case 3:
-                isValidPassword = _a.sent();
+                isValidPassword = _b.sent();
                 //if password is not valid
                 if (isValidPassword === false) {
                     return [2 /*return*/, res.status(401).json({ msg: 'Invalid Credentials' })];
                 }
                 token = assignToken(user);
-                res.json({ msg: 'login successfull', token: token, user: user });
+                _a = user.toObject(), password = _a.password, userWithoutPassword = __rest(_a, ["password"]);
+                res.json({
+                    msg: 'login successfull',
+                    token: token,
+                    user: userWithoutPassword,
+                });
                 return [3 /*break*/, 5];
             case 4:
-                err_1 = _a.sent();
+                err_1 = _b.sent();
                 res.status(400).json({ err: err_1 });
                 return [3 /*break*/, 5];
             case 5: return [2 /*return*/];
--- dist/controllers/inventoryItem/restockInventoryItemController.js
+++ dist/controllers/inventoryItem/restockInventoryItemController.js
@@ -37,16 +37,28 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.restockInventoryItemController = void 0;
+var inventoryItemValidator_1 = require("../../utils/validators/inventoryItemValidator");
 var restockInventoryItemController = function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var inventoryItem, err_1;
     return __generator(this, function (_a) {
-        try {
-            //1. validate request body
-            res.status(201).json({});
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                //1. validate request body
+                return [4 /*yield*/, (0, inventoryItemValidator_1.restockInventoryItemValidator)(req.body)];
+            case 1:
+                //1. validate request body
+                _a.sent();
+                inventoryItem = req.body.inventoryItem;
+                //3. after step 2 is completed successfully, create restockHistory document
+                res.status(201).json({});
+                return [3 /*break*/, 3];
+            case 2:
+                err_1 = _a.sent();
+                res.status(400).json({ err: err_1 });
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
         }
-        catch (err) {
-            res.status(400).json({ err: err });
-        }
-        return [2 /*return*/];
     });
 }); };
 exports.restockInventoryItemController = restockInventoryItemController;
